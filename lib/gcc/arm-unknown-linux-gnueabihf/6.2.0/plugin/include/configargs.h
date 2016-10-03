2676abbba952fde1d98fa751a03621e3553bddd0
Timo Sandmann <mail@timosandmann.de>
2016-10-03 18:39:02 +0200

    gcc 6.2.0, glibc 2.24, binutils 2.26, gdb 7.11.1

---
+++ lib/gcc/arm-unknown-linux-gnueabihf/6.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/arm-linux-toolchain/rpi2/build/src/gcc-6.2.0/configure --build=x86_64-build_apple-darwin16.0.0 --host=x86_64-build_apple-darwin16.0.0 --target=arm-unknown-linux-gnueabihf --prefix=/usr/local/arm-unknown-linux-gnueabihf --with-sysroot=/usr/local/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot --enable-languages=c,c++ --with-cpu=cortex-a7 --with-fpu=vfpv4 --with-float=hard --with-pkgversion='crosstool-NG crosstool-ng-1.22.0-234-g531eab4' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --with-gmp=/Volumes/arm-linux-toolchain/rpi2/build/arm-unknown-linux-gnueabihf/buildtools --with-mpfr=/Volumes/arm-linux-toolchain/rpi2/build/arm-unknown-linux-gnueabihf/buildtools --with-mpc=/Volumes/arm-linux-toolchain/rpi2/build/arm-unknown-linux-gnueabihf/buildtools --with-isl=/Volumes/arm-linux-toolchain/rpi2/build/arm-unknown-linux-gnueabihf/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-linker-build-id --disable-nls --disable-multilib --with-local-prefix=/usr/local/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a7" }, { "float", "hard" }, { "fpu", "vfpv4" }, { "tls", "gnu" } };
